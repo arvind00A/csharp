043ef5ef8179dd32e792f8c818c0dc3bc5f7f482
arvind00A <arvind.kr0024@gmail.com>
2026-03-02 18:20:18 +0530

    Control Statements

---
+++ ControlStatements/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+using ControlStatements;
+
+Console.WriteLine("Hello, dev!");
+
+// if – else if – else
+IfElse ifElse = new IfElse();
+ifElse.IfElseTest();
+ifElse.NestedIfElseTest();
+
+
+// switch – case (including break & default)
+SwitchCase switchCase = new SwitchCase();
+switchCase.SwitchCaseTest();
+switchCase.SwitchExpressionTest();
+switchCase.SwitchPatternMatching();
