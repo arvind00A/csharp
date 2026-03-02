9b215fdfe5010b3bbbfcc9b80ba61baff8814388
arvind00A <arvind.kr0024@gmail.com>
2026-03-02 17:38:06 +0530

    Operator topic deep dive learn

---
+++ Operators/Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Operators;
+
+Console.WriteLine("Hello, dev!");
+
+// Arithmatic Operators
+Arithmatic arithmetic = new Arithmatic();
+Console.WriteLine("\nArithmatic Operators");
+arithmetic.ArithmaticTest();
+
+// Logical Operators
+Logical logical = new Logical();
+Console.WriteLine("\nLogical Operators\n");
+logical.LogicalTest();
+
+
+// Relational Operators
+Relational relational = new Relational();
+Console.WriteLine("\nRelational Operators");
+relational.RelationalTest();
