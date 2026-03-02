4f6fdcea4845966bb2222cf4d6504b0d88e19d78
arvind00A <arvind.kr0024@gmail.com>
2026-03-02 21:34:40 +0530

    loop practice

---
+++ loops/Program.cs
@@ -0,0 +1,25 @@
+﻿// See https://aka.ms/new-console-template for more information
+using loops;
+
+Console.WriteLine("Hello, dev!");
+
+// for loop
+Console.WriteLine("for loop");
+Forloop loop =  new Forloop();
+loop.ForloopTest();
+
+// while loop
+Console.WriteLine("\nwhile loop");
+Whileloop whileLoop = new Whileloop();
+whileLoop.WhileloopTest();
+whileLoop.WhileInfiniteLoop();
+
+// do-while loop
+Console.WriteLine("\ndo-while loop");
+Dowhile dowhile = new Dowhile();
+dowhile.DowhileTest();
+
+// foreach loop
+Console.WriteLine("\nforeach loop");
+Foreachloop foreachloop = new Foreachloop();
+foreachloop.ForeachloopTest();
