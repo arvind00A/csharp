bbeea41b964e054d9b4d7bd1f8b62071582ed1eb
arvind00A <arvind.kr0024@gmail.com>
2026-03-03 18:59:29 +0530

    learn different types of parameters passing

---
+++ MethodsParams/ParamenterPassing.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MethodsParams
+{
+    internal class ParamenterPassing
+    {
+        public void Increment(int x)
+        {
+            x++;
+        }
+
+        public void IncrementRef(ref int x)
+        {
+            x++;
+        }
+
+        public void Calculate(int a, int b, out int sum, out int diff)
+        {
+            sum = a + b;        // Must assign value
+            diff = a - b;       // Must assign value
+        }
+    }
+}
