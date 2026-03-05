5fbc34ba527e806d77819b4ddd49db2db8616d7a
arvind00A <arvind.kr0024@gmail.com>
2026-03-05 16:52:33 +0530

    Fields revise

---
+++ Fields/Employee.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Fields
+{
+    internal class Employee
+    {
+        // Instance fields
+        public string Name;
+        public int Age;
+
+        // Readonly instance field
+        public readonly string EmployeeId;
+
+        // Static field
+        public static string CompanyName = "Tech Solutions Inc.";
+        public static int TotalEmployees = 0;
+
+        public Employee(string name, int age, string id)
+        {
+            Name = name;
+            Age = age;
+            EmployeeId = id;    // readonly set here
+            TotalEmployees++;   // update static counter when a new employee is created
+        }
+    }
+}
---
+++ Fields/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Fields;
+
+Employee emp1 = new Employee("Alice", 30, "E001");
+Employee emp2 = new Employee("Bob", 25, "E002");
+
+Console.WriteLine($"{emp1.Name} works at {Employee.CompanyName}");  // TechCorp
+Console.WriteLine($"Total employees: {Employee.TotalEmployees}");   // 2
